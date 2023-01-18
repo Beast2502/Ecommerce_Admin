3595af56d6237dcc209abfe37d9540e1733f58d1
Mehul Saxena <mehulsaxena45@gmail.com>
2023-01-18 21:04:15 +0530

    Login view completed

--- src/view/Login/login.js
+++ src/view/Login/login.js
@@ -3,40 +3,45 @@ import { connect } from "react-redux";
 import Button from "../../components/Button/Button";
 import "./login.css";
 
-import * as actions from '../../redux/actions';
+import * as actions from "../../redux/actions";
+
+import Image from './image.svg';
 
 const Login = (props) => {
-  const [userEmail ,setUserEmail]  = useState('');
-  const [password,setPassword] = useState('');
+  const [userEmail, setUserEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const handleEmail = (event) =>{
-    setUserEmail(event.target.value)
-  }
+  const handleEmail = (event) => {
+    setUserEmail(event.target.value);
+  };
 
-  const handlePass = (event) =>{
-    setPassword(event.target.value)
-  }
+  const handlePass = (event) => {
+    setPassword(event.target.value);
+  };
 
-  const handleSubmit = (event) =>{
+  const handleSubmit = (event) => {
     event.preventDefault();
-    props.authUser({userEmail,password})
-    console.log(props.isActive)
-  } 
+    props.authUser({ userEmail, password });
+    console.log(props.isActive);
+  };
 
   return (
-   <div>
-        <p>Login View</p>
-        <input type='text' placeholder="Email" onChange={handleEmail} />
-        <input type='password' placeholder="Password" onChange={handlePass} />
-        <Button name={"Login"} color={"orange"} onClick={handleSubmit} />
-   </div>
+    <div className="login-container">
+      <img src={Image} className="login-image"/>
+      <div className="login-detail">
+        <p className="login-title">Admin Login</p>
+        <input type="text" placeholder="Email" onChange={handleEmail}  className="login-control"/>
+        <input type="password" placeholder="Password" onChange={handlePass}  className="login-control"/>
+        <Button name={"Login"} color={"#6c63ff"} onClick={handleSubmit} />
+      </div>
+    </div>
   );
 };
 
-const mapStateToProps = (state)=>{
-  return{
-    isActive : state.isActive
-  }
-}
+const mapStateToProps = (state) => {
+  return {
+    isActive: state.isActive,
+  };
+};
 
-export default connect(mapStateToProps,actions)(Login);
+export default connect(mapStateToProps, actions)(Login);
